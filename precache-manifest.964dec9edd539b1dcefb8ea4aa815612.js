8950b2ce44628fa53f16896d613a7eb9191986af
Cristian Ramirez <ramirezcristian81@live.com>
2020-10-10 15:51:34 -0700

    Updates

---
+++ precache-manifest.964dec9edd539b1dcefb8ea4aa815612.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64a3eec6487f3c50bd82e6e42b7026ef",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "822da67f0b81be0f5f9b",
+    "url": "/portfolio/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "2c50d5560064dfef2211",
+    "url": "/portfolio/static/css/main.b5f6c420.chunk.css"
+  },
+  {
+    "revision": "822da67f0b81be0f5f9b",
+    "url": "/portfolio/static/js/2.dce9a316.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.dce9a316.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c50d5560064dfef2211",
+    "url": "/portfolio/static/js/main.b9e8b4f4.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
