73351765ea48936dda99fa2bf5e0ece7746b0ce3
Cristian Ramirez <ramirezcristian81@live.com>
2020-10-11 05:42:29 -0700

    react router added, routes added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,24 @@ import React from "react";
 import "./App.css";
 import "tachyons";
 import Home from "./pages/Home";
+import About from "./pages/About";
+import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
+import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path="/" exact component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/portfolio" component={Portfolio} />
+            <Route path="/Contact" component={Contact} />
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
